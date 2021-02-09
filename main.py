f854f7e96ea34b7f91c889814a1ed2207b808ca0
tanya0306 <tanya.ssinha@gmail.com>
2021-02-09 16:32:37 -0500

    updates to an existing file

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, send_file, request, url_for, redirect
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 users_info = {}
+saved_file_name = ''
 
 
-def display(user_info):
+@app.route("/download/<filename>")
+def download(filename):
+    path = filename
+    return send_file(path, as_attachment=True, attachment_filename='')
+
+
+def display(user_info, file):
+    print(file, "ii amm file")
+    f = open(file, "r")
+    data = f.read()
+    dataword = data.split()
+    wordcount = len(dataword)
     return render_template("display.html", first_name=user_info["first_name"],
                            last_name=user_info["last_name"], email=user_info["email"],
-                           user_name=user_info["user_name"], password=user_info["password"]
-                           )
+                           user_name=user_info["user_name"], password=user_info["password"],
+                           filename=file, wordcount=wordcount)
 
 
 @app.route("/")
@@ -24,7 +37,8 @@ def login():
         if user_name.lower() in users_info.keys():
             user_info = users_info[user_name.lower()]
             if password == user_info["password"]:
-                return display(user_info)
+                global saved_file_name
+                return display(user_info,saved_file_name)
             else:
                 return "Password is incorrect"
         else:
@@ -36,6 +50,10 @@ def login():
 @app.route('/register', methods=['POST', "GET"])
 def register():
     if request.method == "POST":
+        filename = request.files['file']
+        filename.save(filename.filename)
+        global saved_file_name
+        saved_file_name = filename.filename
         user_info = {
             "first_name": request.form['firstname'],
             "last_name": request.form['lastname'],
@@ -45,7 +63,7 @@ def register():
         }
         users_info[request.form["username"].lower()] = user_info
         print("the datastore dict now has these values {}\n".format(users_info))
-        return display(user_info)
+        return display(user_info, saved_file_name)
     if request.method == "GET":
         return render_template("register.html")
 
